05a6eca2a86d700243dc31086fd9ed5a16686aca
Fantomas4 <stef1997s@gmail.com>
2018-03-26 07:28:58 +0300

    Continued work on the project.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def transmit_msg(msg):
 
     for bit in range(len(msg)):
@@ -9,12 +10,55 @@ def transmit_msg(msg):
 
     return msg
 
-def calculate_CRC_code(p,msg):
+
+def modulo2_operation(a, b):
+
+    result_bit_num = []
+
+    from operator import xor
+
+    for bit in range(len(a)):
+        result_bit_num.append(xor(bool(a[bit]), bool(b[bit])))
+
+    return result_bit_num
 
 
-def generate_final_message_with_CRC_code(p,msg):
+def calculate_crc_code(p,msg):
 
-    CRC_code = calculate_CRC_code(p,msg)
+    # p number is n+1
+    n = p - 1
+
+    # add n 0's to the end of the msg
+    for i in range(n):
+        msg.append(0)
+
+    # calculate the FCS number
+
+    pos = n-1
+    temp_bit_num = msg
+
+    # for bit in range(0, n-1):
+    #     temp_bit_num.append(msg[bit])
+
+    while pos != len(msg):
+
+        temp_bit_num = modulo2_operation(temp_bit_num, p)
+
+        for bit in range(len(temp_bit_num)):
+            # remove any 0's from the front of the number ex. 00010101 --> 10101
+            if temp_bit_num[bit] == 0:
+                del temp_bit_num[bit]
+
+        pos += 1
+
+        # append the next bit of the original message to the end of the temp bit number used for the modulo 2
+        temp_bit_num.append(msg[pos])
+
+
+def generate_final_message_with_crc_code(p,msg):
+
+    CRC_code = calculate_crc_code(p,msg)
+
 
 def generate_random_message(k):
 
@@ -29,13 +73,13 @@ def generate_random_message(k):
 
 def main():
 
-    p_number = input("> Enter the P number (bits) you want to use: ")
-    k_number = input("> Enter the k number (the length (amount of bits) of the messages to be transmitted: ")
-    msg_amount = input("> Enter the amount of messages that should be transmitted during the simulation: ")
+    p_number = int(input("> Enter the P number (bits) you want to use: "))
+    k_number = int(input("> Enter the k number (the length (amount of bits) of the messages to be transmitted: "))
+    msg_amount = int(input("> Enter the amount of messages that should be transmitted during the simulation: "))
 
     for i in range(msg_amount):
 
-        generated_msg = generate_random_message()
+        generated_msg = generate_random_message(k_number)
         received_msg = transmit_msg(generated_msg)
 
 
