31ed1e3f914e38b499567cd599838a8588a54eb2
Fantomas4 <stef1997s@gmail.com>
2018-03-27 06:09:34 +0300

    Continued work on the project.

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def check_received_msg_integrity(msg, p):
+class TransmissionInfoLog:
+
+    def __init__(self, total_msg_transferred):
+        self.incorrect_msg_transferred_count = 0
+        self.incorrect_msg_detected_count = 0
+        self.total_msg_transferred = total_msg_transferred
+
+    def inc_detected_count(self):
+        # increase the counter for the messages that were detected to have been transmitted with errors.
+        self.incorrect_msg_detected_count += 1
+
+    def inc_actual_count(self):
+        # increase the counter of the messages that were truly transmitted with errors
+        self.incorrect_msg_transferred_count += 1
+
+
+def check_received_msg_integrity(msg, p, log):
+
     remainder = perform_modulo2_operation(msg, p)
 
+    if remainder is 0:
+        pass
+    else:
+        log.inc_detected_count()
 
-def transmit_msg(msg):
+
+def transmit_msg(msg, log):
+
+    error_in_msg = False
 
     for bit in range(len(msg)):
 
@@ -10,6 +34,10 @@ def transmit_msg(msg):
 
         if random.randint(1, 1000) == 355:
             msg[bit] = not msg[bit]
+            error_in_msg = True
+
+    if error_in_msg is True:
+        log.inc_actual_count()
 
     return msg
 
@@ -88,13 +116,20 @@ def main():
     k_number = int(input("> Enter the k number (the length (amount of bits) of the messages to be transmitted: "))
     msg_amount = int(input("> Enter the amount of messages that should be transmitted during the simulation: "))
 
+    transmission_log = TransmissionInfoLog(msg_amount)
+
     for i in range(msg_amount):
 
         generated_msg = generate_random_message(k_number)
         final_msg_with_crc_code = generate_final_message_with_crc_code(generated_msg, p_number)
-        received_msg = transmit_msg(final_msg_with_crc_code)
+        received_msg = transmit_msg(final_msg_with_crc_code, transmission_log)
 
-        msg_verification_success = check_received_msg_integrity(received_msg, p_number)
+        check_received_msg_integrity(received_msg, p_number, transmission_log)
+
+    print("\n\n\n=============== Transmission Log Results ===============")
+    print("* Total amount of messages transmitted: ", msg_amount)
+    print("* Total amount of messages that were actually transmitted containing bit errors: ", transmission_log.incorrect_msg_transferred_count)
+    print("* Total amount of messages that were correctly detected by the CRC as corrupted: ", transmission_log.incorrect_msg_detected_count)
 
 
 if __name__ == "__main__":
